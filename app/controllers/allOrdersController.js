0d8895744aad149d63ca6e5689a3b82c792dec1b
Travis <mail@travisbloomfield.com>
2016-03-24 15:01:52 +1100

    Show total of all orders.

--- app/controllers/allOrdersController.js
+++ app/controllers/allOrdersController.js
@@ -4,21 +4,28 @@
 
   function AllOrdersController($scope, customersService, $log) {
     var allOrders = [];
+    $scope.allOrdersTotal = 0;
+    $scope.totalClass = 'danger';
 
     var customers = customersService.getCustomers()
-    .then(function(response) {
-      var customers = response.data;
-      customers.forEach(function(customer) {
-        customer.orders.forEach(function(order) {
-          allOrders.push(order);
+      .then(function(response) {
+        var customers = response.data;
+        customers.forEach(function(customer) {
+          customer.orders.forEach(function(order) {
+            allOrders.push(order);
+          });
         });
+        $scope.allOrders = allOrders;
+
+        $scope.allOrdersTotal = $scope.allOrders.reduce(function(total, current) {
+          return total + parseInt(current.total);
+        }, 0);
+
+        $scope.totalClass = ($scope.allOrdersTotal > 300) ? 'success' : 'danger';
+
+      }, function(response) {
+        $log.log('[allOrdersController] Error getting customers: ' + response.status + ' ' + statusText);
       });
-      $scope.allOrders = allOrders;
-
-    }, function(response) {
-      $log.log('[allOrdersController] Error getting customers: ' + response.status + ' ' + statusText);
-    });
-
   }
 
   angular.module('customersApp').controller('AllOrdersController', AllOrdersController);
