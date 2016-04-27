f2288ae696eab451d2a6fbd4333933499ab48ee7
Travis <mail@travisbloomfield.com>
2016-04-27 17:01:35 +1000

    Improve comments and remove unused console log.

--- app/controllers/addCustomerController.js
+++ app/controllers/addCustomerController.js
@@ -1,24 +1,24 @@
+/* Adds a customer to the backend */
+
 (function() {
   'use strict';
 
   AddCustomerController.$inject = ['$scope', '$window', 'customersService'];
 
   function AddCustomerController($scope, $window, customersService) {
+    // Initialise blank values for input fields.
     $scope.name = '';
     $scope.city = '';
 
+    // Create a new customer by sending a POST request to the backend.
     $scope.createCustomer = function() {
-      console.log('creating');
       // POST new customer
       customersService.createCustomer($scope.name, $scope.city)
         .then(function(response) {
           if (response.status === 200) {
             $window.location.href = '/';
           }
-
-
         });
-
     };
   }
 
