a2851ad795ebf977b943ce9ba51904e098e817ed
Travis <mail@travisbloomfield.com>
2016-04-27 17:27:43 +1000

    Improve comments.

--- app/services/customersService.js
+++ app/services/customersService.js
@@ -1,18 +1,24 @@
+/* Interacts with backend to provide customer data interaction. */
+
 (function() {
   var customersService = function($http) {
 
+    // Get all customers
     this.getCustomers = function() {
       return $http.get('http://localhost:3000/api/customers');
     };
 
+    // Get a specific customer
     this.getCustomer = function(customerId) {
       return $http.get('http://localhost:3000/api/customers/' + customerId);
     };
 
+    // Delete a specific customer
     this.deleteCustomer = function(customerId) {
       return $http.delete('http://localhost:3000/api/customers/' + customerId);
     };
 
+    // Create a new customer
     this.createCustomer = function(name, city) {
       return $http.post('http://localhost:3000/api/customers/', {
         name: name,
--- app/services/productsService.js
+++ app/services/productsService.js
@@ -1,11 +1,14 @@
+/* Interacts with backend to provide services for products */
 (function() {
   var productsService = function($http) {
 
+    // Get all products
     this.getProducts = function() {
       return $http.get('http://localhost:3000/api/products');
     };
 
 
+    // Get a specific product.
     // TODO Cache all proucts locally to prevent lots of requests.
     this.getProduct = function(id) {
       return $http.get('http://localhost:3000/api/products/' + id);
